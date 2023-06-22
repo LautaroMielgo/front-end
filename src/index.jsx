6f41f40887a1ee2262bb3717b8f996709633479a
Lautaro Mielgo <lautimielgo@gmail.com>
2023-06-22 20:50:13 -0300

    deplot front

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM ,{createRoot} from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
@@ -12,7 +12,8 @@ axios.defaults.baseURL = 'https://back-end-production-913f.up.railway.app/';
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
  <BrowserRouter>
   <Provider store={store}>
