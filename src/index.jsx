65bf1e575baa0bbd26fdf06bedc1498b3c4e21ce
Lautaro Mielgo <lautimielgo@gmail.com>
2023-06-22 19:48:17 -0300

    deploy front

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,12 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import store from './redux/store.js'
+import axios from 'axios';
+
+// axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://back-end-production-913f.up.railway.app/';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,16 +1,19 @@
 import { GET_ALL_POSTS, GET_ID_POST, CREATE_COMPANY } from "./action-types";
 import axios from "axios";
 
+
+
+
 export const getAllPosts = () => {
     return async function (dispatch){
-        const {data} = await axios.get(`https://back-end-production-913f.up.railway.app/posts`);
+        const {data} = await axios.get(`/posts`);
         const posts = data;
         dispatch({ type: GET_ALL_POSTS, payload: posts });
     }
 }
 export const getPostById = (id) => {
     return async function (dispatch) {
-        const {data} = await axios.get(`http://localhost:3001/posts/${id}`);
+        const {data} = await axios.get(`/posts/${id}`);
         const post = data;
         dispatch({ type: GET_ID_POST, payload: post });        
     }
@@ -28,7 +31,7 @@ export const getCompanies = () => {
 
 export const createCompany = (payload) => {
     return async function (dispatch) {
-        await axios.post("http://localhost:3001/company/", payload)
+        await axios.post("/company/", payload)
         return dispatch({type: CREATE_COMPANY})
     }
 }
