1de95a8d942095dfd0e63faca4213cd67f791350
Lautaro Mielgo <lautimielgo@gmail.com>
2023-06-23 12:43:12 -0300

    react links

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavLink } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 const SideBar = () => {
   const [selectedField, setSelectedField] = useState('tech-news');
@@ -10,21 +10,21 @@ const SideBar = () => {
       <div className='row'>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark col-1 position-fixed vh-100 p-0 align-to mt-5 bg-transparent">
           <div className="navbar-nav flex-column h-200 d-flex ml-6 ">
-            <a href='/home' className={`nav-link ${selectedField === 'tech-news' ? 'active' : ''}`} onClick={() => setSelectedField('tech-news')}>
-              <i className="bi bi-newspaper fs-4  text-success" ></i>
-            </a>
+            <Link to='/home' className={`nav-link ${selectedField === 'tech-news' ? 'active' : ''}`} onClick={() => setSelectedField('tech-news')}>
+              <i className="bi bi-newspaper fs-4 text-success" ></i>
+            </Link>
     
-            <a href='/community' className={`nav-link ${selectedField === 'community' ? 'active' : ''}`} onClick={() => setSelectedField('community')}>
+            <Link to='/community' className={`nav-link ${selectedField === 'community' ? 'active' : ''}`} onClick={() => setSelectedField('community')}>
               <i className="bi bi-people fs-4 text-success"></i>
-            </a>
+            </Link>
 
-            <a href='/JobsOffers' className={`nav-link ${selectedField === 'job-applications' ? 'active' : ''}`} onClick={() => setSelectedField('job-applications')}>
+            <Link to='/JobsOffers' className={`nav-link ${selectedField === 'job-applications' ? 'active' : ''}`} onClick={() => setSelectedField('job-applications')}>
               <i className="bi bi-briefcase fs-4 text-success "></i>
-            </a>
+            </Link>
 
-            <a className={`nav-link ${selectedField === 'settings' ? 'active' : ''}`} onClick={() => setSelectedField('settings')}>
+            <Link to='/settings' className={`nav-link ${selectedField === 'settings' ? 'active' : ''}`} onClick={() => setSelectedField('settings')}>
               <i className="bi bi-gear fs-4 text-success"></i>
-            </a>
+            </Link>
           </div>
         </nav>
       </div>
@@ -33,4 +33,5 @@ const SideBar = () => {
 }
 
 export default SideBar;
+
 
