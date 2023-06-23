95d0488070121efe5e62e5f4cacdfcfb2ebbd35e
Lautaro Mielgo <lautimielgo@gmail.com>
2023-06-23 12:06:12 -0300

    cambio de etiqueta a por Link

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom"
 
 const SideBar = () => {
   const [selectedField, setSelectedField] = useState('tech-news');
@@ -9,9 +10,9 @@ const SideBar = () => {
       <div className='row'>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark col-1 position-fixed vh-100 p-0 align-to mt-5 bg-transparent">
           <div className="navbar-nav flex-column h-200 d-flex ml-6 ">
-            <a href='/home' className={`nav-link ${selectedField === 'tech-news' ? 'active' : ''}`} onClick={() => setSelectedField('tech-news')}>
+            <Link href='/home' className={`nav-link ${selectedField === 'tech-news' ? 'active' : ''}`} onClick={() => setSelectedField('tech-news')}>
               <i className="bi bi-newspaper fs-4  text-success" ></i>
-            </a>
+            </Link>
     
             <a href='/community' className={`nav-link ${selectedField === 'community' ? 'active' : ''}`} onClick={() => setSelectedField('community')}>
               <i className="bi bi-people fs-4 text-success"></i>
