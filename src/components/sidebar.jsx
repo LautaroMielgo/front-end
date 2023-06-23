895b45e20373fd8a797e25a955a7dd1c6f7cb86f
Lautaro Mielgo <lautimielgo@gmail.com>
2023-06-23 12:25:17 -0300

    revierto todos los cambios

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -10,15 +10,15 @@ const SideBar = () => {
       <div className='row'>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark col-1 position-fixed vh-100 p-0 align-to mt-5 bg-transparent">
           <div className="navbar-nav flex-column h-200 d-flex ml-6 ">
-            <a href='/home' className={`link ${selectedField === 'tech-news' ? 'active' : ''}`} onClick={() => setSelectedField('tech-news')}>
+            <a href='/home' className={`nav-link ${selectedField === 'tech-news' ? 'active' : ''}`} onClick={() => setSelectedField('tech-news')}>
               <i className="bi bi-newspaper fs-4  text-success" ></i>
             </a>
     
-            <a href='/community' className={`link ${selectedField === 'community' ? 'active' : ''}`} onClick={() => setSelectedField('community')}>
+            <a href='/community' className={`nav-link ${selectedField === 'community' ? 'active' : ''}`} onClick={() => setSelectedField('community')}>
               <i className="bi bi-people fs-4 text-success"></i>
             </a>
 
-            <a href='/JobsOffers' className={`link ${selectedField === 'job-applications' ? 'active' : ''}`} onClick={() => setSelectedField('job-applications')}>
+            <a href='/JobsOffers' className={`nav-link ${selectedField === 'job-applications' ? 'active' : ''}`} onClick={() => setSelectedField('job-applications')}>
               <i className="bi bi-briefcase fs-4 text-success "></i>
             </a>
 
