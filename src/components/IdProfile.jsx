63e72e4358f45a468d3bbd7b8139b23047dc5589
Lautaro Mielgo <lautimielgo@gmail.com>
2023-07-05 15:39:53 -0300

    idprofile

--- src/components/IdProfile.jsx
+++ src/components/IdProfile.jsx
@@ -23,8 +23,16 @@ export const IdProfile = () => {
     <div>
       {user.user_datum ? (
         <div className="flex items-center justify-center w-screen h-screen bg-teal-500 bg-opacity-30">
-          <div className="ml-[180px] mt-14 bg-white shadow-md rounded-lg overflow-hidden w-[80%] h-[80%]">
+          <div className="ml-[180px] mt-14 bg-white shadow-md rounded-lg overflow-hidden w-[80%] h-[90%]">
             <div className="p-4">
+              {user.user_datum.rol === "developer" ? 
+              <button
+                className="ml-[90%] mt-0 bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={() => (window.location.href = "/devdata")}
+              >
+                DevData CV
+              </button>: null}
+
               <div className="flex items-center">
                 <img
                   src={user.user_datum.image.url}
@@ -41,10 +49,53 @@ export const IdProfile = () => {
                 </div>
               </div>
               <div className="mt-4 items-center justify-center ml-4">
-                {user.user_datum.rol_type === "user" ? (
-                  <h3 className="text-2xl font-semibold ml-[20%]">
-                    Developer Information
-                  </h3>
+                {user.user_datum.rol === "developer" ? (
+                  <>
+                    <h3 className="text-2xl font-semibold ml-[20%]">
+                      Developer Information
+                    </h3>
+                    <p>
+                      <span className="font-medium text-xl font-mono">
+                        Experience:
+                      </span>{" "}
+                      {filteredUser[0]?.user_datum.devdatum?.experience.puesto} at{" "}
+                      {filteredUser[0]?.user_datum.devdatum?.experience.empresa} (
+                      {filteredUser[0]?.user_datum.devdatum?.experience.duracion})
+                    </p>
+                    <p>
+                      <span className="font-medium text-xl font-mono">
+                        Education:
+                      </span>{" "}
+                      {filteredUser[0]?.user_datum.devdatum?.education.titulo} at{" "}
+                      {filteredUser[0]?.user_datum.devdatum?.education.institucion} (
+                      {filteredUser[0]?.user_datum.devdatum?.education.año})
+                    </p>
+                    <p>
+                      <span className="font-medium text-xl font-mono">
+                        Skills:
+                      </span>{" "}
+                      {filteredUser[0]?.user_datum.devdatum?.skills.join(", ")}
+                    </p>
+                    <p>
+                      <span className="font-medium text-xl font-mono">
+                        Ratings:
+                      </span>{" "}
+                      {filteredUser[0]?.user_datum.devdatum?.ratings}
+                    </p>
+                    <p>
+                      <span className="font-medium text-xl font-mono">
+                        Curriculum Vitae:
+                      </span>{" "}
+                      <a
+                        href={filteredUser[0]?.user_datum.devdatum?.curriculumVitae.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="bg-teal-500 py-2 px-2 font-mono rounded-xl"
+                      >
+                        View CV
+                      </a>
+                    </p>
+                  </>
                 ) : (
                   <h3 className="text-2xl font-semibold ml-[20%]">
                     Company Information
@@ -61,8 +112,9 @@ export const IdProfile = () => {
                   <span className="font-medium text-xl font-mono">
                     Date of Birth:
                   </span>{" "}
-                  {user.user_datum.date_birthday}
+                  {user.user_datum.date_birthday.split("T")[0]}
                 </p>
+
                 <p>
                   <span className="font-medium text-xl font-mono">
                     Address:
@@ -86,9 +138,9 @@ export const IdProfile = () => {
               </div>
             </div>
             <div className="px-4 py-2 bg-gray-200">
-              <h3 className="text-lg font-semibold">Posts</h3>
+              <h3 className="text-lg font-semibold mt-0">Posts</h3>
               {user.user_datum.posts && user.user_datum.posts.length > 0 ? (
-                <ul className="mt-2">
+                <ul className="mt-2 z-20">
                   {user.user_datum.posts.map((post, index) => (
                     <li key={index} className="mb-2">
                       <h4>{post.title}</h4>
