c394e0b88cbb38e1043c8ffb6a4ebd6dc6f5f70b
Lautaro Mielgo <lautimielgo@gmail.com>
2023-07-06 00:20:30 -0300

    agrego fotitos

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -157,7 +157,7 @@ export const Register = () => {
                   type="password"
 
                   id="floating_outlined2"
-                  className="block px-2.5 pb-2.5 pt-4 w-full sm:w-[215%] text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-black font-mono dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className="block px-2.5 pb-2.5 pt-4 w-full sm:w-[100%] text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-black font-mono dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 
                   placeholder=" "
                   name="password"
@@ -182,7 +182,7 @@ export const Register = () => {
                   <input
                     type="password"
                     id="floating_outlined2"
-                    className="block px-2.5 pb-2.5 pt-4 w-full sm:w-[300%] text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-black font-mono dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    className="block px-2.5 pb-2.5 pt-4 w-full sm:w-[223%] text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-black font-mono dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     name="password2"
                     value={confirmPassword.password2}
