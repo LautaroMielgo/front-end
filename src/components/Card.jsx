429ed0f00f3396bdfd2880277cb5189bc7557cc4
Lautaro Mielgo <lautimielgo@gmail.com>
2023-07-06 19:24:30 -0300

    agrego fotitos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,45 +21,78 @@ const Card = ({ post }) => {
   }
 
   return (
-    <div className="card mb-3" key={post.id}>
-      <div className="row g-0">
-        <div className="col-md-4">
-          {post.image && <img src={post.image.url} alt={post.title} className="img-fluid rounded-start" />}
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title text-2x1 font-bold text-black">{post.title}</h5>
-            <p className="card-text">{post.body}</p>
-            <div className="space-x-6">
-              {user.user_datum?.rol === "admin"?<button type="button" className="btn btn-dark text-dark color-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Eliminar
-              </button> : null}
+    // <div className="card mb-3" key={post.id}>
+    //   <div className="row g-0">
+    //     <div className="col-md-4">
+    //       {post.image && <img src={post.image.url} alt={post.title} className="img-fluid rounded-start" />}
+    //     </div>
+    //     <div className="col-md-8">
+    //       <div className="card-body">
+    //         <h5 className="card-title text-2x1 font-bold text-black">{post.title}</h5>
+    //         <p className="card-text">{post.body}</p>
+    //         <div className="space-x-6">
+    //           {user.user_datum?.rol === "admin"?<button type="button" className="btn btn-dark text-dark color-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    //             Eliminar
+    //           </button> : null}
               
-            </div>
-            <p className="card-text">
-            </p>
-          </div>
-        </div>
-      </div>
+    //         </div>
+    //         <p className="card-text">
+    //         </p>
+    //       </div>
+    //     </div>
+    //   </div>
 
-      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">Confirmación</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              <p>¿Estás seguro de que deseas eliminar este posteo?</p>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-dark text-dark" data-bs-dismiss="modal">Cancelar</button>
-              <button type="button" className="btn btn-danger text-dark" onClick={handleDelete}>Eliminar</button>
-            </div>
+    //   <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    //     <div className="modal-dialog">
+    //       <div className="modal-content">
+    //         <div className="modal-header">
+    //           <h1 className="modal-title fs-5" id="exampleModalLabel">Confirmación</h1>
+    //           <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    //         </div>
+    //         <div className="modal-body">
+    //           <p>¿Estás seguro de que deseas eliminar este posteo?</p>
+    //         </div>
+    //         <div className="modal-footer">
+    //           <button type="button" className="btn btn-dark text-dark" data-bs-dismiss="modal">Cancelar</button>
+    //           <button type="button" className="btn btn-danger text-dark" onClick={handleDelete}>Eliminar</button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+   
+    <div className="card text-bg-dark mb-3">
+    {post.image && <img src={post.image.url} alt={post.title} className="card-img" />}
+    <div className="card-img-overlay">
+      <h5 className="card-title text-white backdrop-blur-sm">{post.title}</h5>
+      <p className="position-absolute bottom-2 card-text text-white backdrop-blur-md">{post.body}</p>
+      <div className="position-absolute bottom-0 end-0">
+        {user.user_datum?.rol === "admin" && (
+          <button type="button" className="btn btn-warning text-dark color-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Eliminar
+          </button>
+        )}
+      </div>
+    </div>
+
+    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5 text-black" id="exampleModalLabel">Confirmación</h1>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            <p className="text-black">¿Estás seguro de que deseas eliminar este posteo?</p>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-dark text-dark" data-bs-dismiss="modal">Cancelar</button>
+            <button type="button" className="btn btn-danger text-dark" onClick={handleDelete}>Eliminar</button>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
